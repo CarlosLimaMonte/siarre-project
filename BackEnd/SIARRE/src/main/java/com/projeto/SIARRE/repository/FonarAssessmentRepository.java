386d5ddaa07e58e6f7aa5596531b70fc5a1053b7
Carlos Eduardo de Lima Monte Filho <carlos.eduardo.limamonte@gmail.com>
2026-09-24 12:14:29 -0300

    FonarAssessmentService created

--- BackEnd/SIARRE/src/main/java/com/projeto/SIARRE/repository/FonarAssessmentRepository.java
+++ BackEnd/SIARRE/src/main/java/com/projeto/SIARRE/repository/FonarAssessmentRepository.java
@@ -1,8 +1,13 @@
 package com.projeto.SIARRE.repository;
 
 import com.projeto.SIARRE.entity.FonarAssessment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FonarAssessmentRepository extends JpaRepository<FonarAssessment, Long> {
+
+  List<FonarAssessment> findByVictim_id(Long victimId);
+
+  List<FonarAssessment> findByAggressor_id(Long aggressorId);
 
 }
---
+++ BackEnd/SIARRE/src/main/java/com/projeto/SIARRE/service/FonarAssessmentService.java
@@ -0,0 +1,111 @@
+package com.projeto.SIARRE.service;
+
+import com.projeto.SIARRE.entity.Aggressor;
+import com.projeto.SIARRE.entity.FonarAssessment;
+import com.projeto.SIARRE.entity.Victim;
+import com.projeto.SIARRE.entity.dto.FonarAssessmentCreateDto;
+import com.projeto.SIARRE.entity.dto.FonarAssessmentDto;
+import com.projeto.SIARRE.exception.AggressorNotFoundException;
+import com.projeto.SIARRE.exception.FonarAssessmentNotFoundException;
+import com.projeto.SIARRE.exception.VictimNotFoundException;
+import com.projeto.SIARRE.repository.AggressorRepository;
+import com.projeto.SIARRE.repository.FonarAssessmentRepository;
+import com.projeto.SIARRE.repository.VictimRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FonarAssessmentService {
+
+  private final FonarAssessmentRepository fonarAssessmentRepository;
+  private final VictimRepository victimRepository;
+  private final AggressorRepository aggressorRepository;
+
+  public FonarAssessmentService(FonarAssessmentRepository fonarAssessmentRepository,
+      VictimRepository victimRepository, AggressorRepository aggressorRepository) {
+    this.fonarAssessmentRepository = fonarAssessmentRepository;
+    this.victimRepository = victimRepository;
+    this.aggressorRepository = aggressorRepository;
+  }
+
+  // Create
+
+  public FonarAssessmentDto createFonarAssessment(FonarAssessmentCreateDto assessmentCreateDto){
+
+    Victim victim = victimRepository.findById(assessmentCreateDto.victimId()).orElseThrow(
+        VictimNotFoundException::new
+    );
+
+    Aggressor aggressor = aggressorRepository.findById(assessmentCreateDto.aggressorId()).orElseThrow(
+        AggressorNotFoundException::new
+    );
+
+    return FonarAssessmentDto.fromEntity(fonarAssessmentRepository.save(assessmentCreateDto.toEntity(victim, aggressor)));
+
+  }
+
+  // Read - All
+
+  public List<FonarAssessmentDto> findAllFonarAssessment(){
+    return fonarAssessmentRepository.findAll().stream().map(
+        FonarAssessmentDto :: fromEntity
+    ).toList();
+  }
+
+  // Read - Find by Id
+
+  public FonarAssessment findById(Long id){
+    return fonarAssessmentRepository.findById(id).orElseThrow(
+        FonarAssessmentNotFoundException :: new
+    );
+  }
+
+  // Read - Find By Victim Id
+
+  public List<FonarAssessmentDto> findByVictimId(Long victimId){
+    return fonarAssessmentRepository.findByVictim_id(victimId).stream().map(
+        FonarAssessmentDto::fromEntity
+    ).toList();
+  }
+
+  // Read - Find by Aggressor Id
+
+
+  public List<FonarAssessmentDto> findByAggressorId(Long aggressorId){
+    return fonarAssessmentRepository.findByAggressor_id(aggressorId).stream().map(
+        FonarAssessmentDto::fromEntity
+    ).toList();
+  }
+
+  // Update - By Id
+
+  public FonarAssessmentDto updateFonarAssessment(Long id, FonarAssessmentCreateDto assessmentCreateDto){
+
+    FonarAssessment assessmentFromDb = findById(id);
+
+    Victim victim = victimRepository.findById(assessmentCreateDto.victimId()).orElseThrow(
+        VictimNotFoundException::new
+    );
+
+    Aggressor aggressor = aggressorRepository.findById(assessmentCreateDto.aggressorId()).orElseThrow(
+        AggressorNotFoundException::new
+    );
+
+    assessmentFromDb.setVictim(victim);
+    assessmentFromDb.setAggressor(aggressor);
+
+    return FonarAssessmentDto.fromEntity(fonarAssessmentRepository.save(assessmentFromDb));
+
+  }
+
+  // Delete - By Id
+
+  public void deleteById(Long id){
+    FonarAssessment assessment = findById(id);
+
+    fonarAssessmentRepository.deleteById(id);
+
+  }
+
+
+}
