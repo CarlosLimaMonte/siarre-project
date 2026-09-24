cbb887063f1d530ceb3065812e88274e2582a919
Carlos Eduardo de Lima Monte Filho <carlos.eduardo.limamonte@gmail.com>
2026-09-24 12:37:10 -0300

    method createFonarAssessment created

--- BackEnd/SIARRE/src/main/java/com/projeto/SIARRE/controller/FonarAssessmentController.java
+++ BackEnd/SIARRE/src/main/java/com/projeto/SIARRE/controller/FonarAssessmentController.java
@@ -2,11 +2,40 @@ package com.projeto.SIARRE.controller;
 
 
 import com.projeto.SIARRE.entity.FonarAssessment;
+import com.projeto.SIARRE.entity.dto.FonarAssessmentCreateDto;
+import com.projeto.SIARRE.entity.dto.FonarAssessmentDto;
+import com.projeto.SIARRE.service.FonarAssessmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/assessment")
 public class FonarAssessmentController {
+
+  private final FonarAssessmentService fonarAssessmentService;
+
+  public FonarAssessmentController(FonarAssessmentService fonarAssessmentService) {
+    this.fonarAssessmentService = fonarAssessmentService;
+  }
+
+  // Create
+
+  @PostMapping
+  public ResponseEntity<FonarAssessmentDto> createFonarAssessment(@RequestBody
+      FonarAssessmentCreateDto assessmentCreateDto){
+
+    FonarAssessmentDto assessmentDto = fonarAssessmentService.createFonarAssessment(assessmentCreateDto);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(assessmentDto);
+  }
+
+  // Read - All
+
+
 
 }
